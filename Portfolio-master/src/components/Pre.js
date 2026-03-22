55657ce8ada56f11257f9d9acf22b7852c82623f
Jayasriramsankarasetty <144556602+Jayasriramsankarasetty@users.noreply.github.com>
2026-03-22 17:32:11 +0530

    Update logo and favicon

--- Portfolio-master/src/components/Pre.js
+++ Portfolio-master/src/components/Pre.js
@@ -1,6 +1,6 @@
 import React from "react";
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+  return <div id={props.load ? "preloader" : "preloader-none"}><img src="pre.svg" alt="preloader" className="img-fluid" /></div>;
 }
 
 export default Pre;
